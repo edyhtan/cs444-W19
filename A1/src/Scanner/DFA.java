09bcf108c5d5e7cd82c5112b1c3a398b46d8c846
Ye Hua Tan <yh2tan@uwaterloo.ca>
2019-01-25 13:59:01 -0500

    intellj files

--- A1/src/Scanner/DFA.java
+++ A1/src/Scanner/DFA.java
@@ -1,7 +1,29 @@
 package Scanner;
 
-public interface DFA {
-    void next(char c);
-    boolean isFinal();
-    String getLexeme();
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.FileSystemNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class DFA {
+    ArrayList<String> states = new ArrayList<>();
+    HashSet<String> finishing = new HashSet<>();
+    HashMap<String, HashMap<Character, String>> transitions = new HashMap<>();
+
+    int currentState = 0;
+
+    public DFA() throws FileNotFoundException{
+        loadDFA();
+    }
+
+    // Read .dfa file
+    private void loadDFA() throws FileNotFoundException {
+        Scanner scan = new Scanner(new File(("src/Scanner/scanner.dfa")));
+
+    }
+
+
 }
