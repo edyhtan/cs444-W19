1d1da57db91c0ba9cf282fea9bd484681f54ffc6
Ye Hua Tan <yh2tan@uwaterloo.ca>
2019-02-06 14:44:09 -0500

    Added Expressions - Minor fix to implementing interface - Added Expression

--- A1/test/Playable.java
+++ A1/test/Playable.java
@@ -3,7 +3,12 @@ package test;
 import java.lang.*;
 import java.io.*;
 
-public class Playbale {
+public class Playbale extends java.lang.Serializable implements Object {
+
+    public int num = 0;
+    protected static Object obj = new Object();
+    public final byte[] arg = new byte[3];
+
     public Playable() {
     }
 }
