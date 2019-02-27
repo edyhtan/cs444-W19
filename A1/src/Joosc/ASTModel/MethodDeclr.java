e1348a44cededafba97708e503b56dc399f58c6d
Suri Zhang <surizhang1224@gmail.com>
2019-02-27 00:00:30 -0500

    ASTModel MethodDeclr

---
+++ A1/src/Joosc/ASTModel/MethodDeclr.java
@@ -0,0 +1,56 @@
+package Joosc.ASTModel;
+
+import Joosc.ASTBuilding.ASTStructures.MethodDeclrNode;
+import Joosc.ASTBuilding.ASTStructures.Statements.StatementNode;
+import Joosc.ASTBuilding.ASTStructures.TypeNode;
+import Joosc.ASTBuilding.Constants.Symbol;
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class MethodDeclr implements ClassMemberDeclr {
+    ArrayList<Symbol> modifiers;
+    TypeNode type;
+    String name;
+    ArrayList<Pair<TypeNode, String>> formalParamList;
+    ArrayList<StatementNode> bodyBlock;
+    String canonicalID;
+
+    public MethodDeclr(MethodDeclrNode node) {
+        modifiers = node.getModifiers();
+        type = node.getType();
+        name = node.getName();
+        formalParamList = node.getFormalParamList();
+        bodyBlock = node.getBodyBlock();
+    }
+
+    @Override
+    public void buildCanonicalName(String className) {
+        StringBuilder sb = new StringBuilder(className + DOT + name + "(");
+
+        String typeList = formalParamList.stream()
+                .map(pair -> pair.getKey().toString())
+                .collect(Collectors.joining(COMMA));
+
+        sb.append(typeList).append(")").append(type.toString());
+
+        canonicalID = sb.toString();
+    }
+
+    private TypeNode getType() {
+        return type;
+    }
+
+    private ArrayList<StatementNode> getBodyBlock() {
+        return bodyBlock;
+    }
+
+    private ArrayList<Pair<TypeNode, String>> getFormalParamList() {
+        return formalParamList;
+    }
+
+    private String getName() {
+        return name;
+    }
+}
