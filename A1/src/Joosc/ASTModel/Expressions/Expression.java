b35e7ad2d8715597276e51f95b7aea5d486538cc
Suri Zhang <surizhang1224@gmail.com>
2019-02-28 11:51:14 -0500

    expressionClassInstanceCreation Model

--- A1/src/Joosc/ASTModel/Expressions/Expression.java
+++ A1/src/Joosc/ASTModel/Expressions/Expression.java
@@ -1,6 +1,5 @@
 package Joosc.ASTModel.Expressions;
 
-import Joosc.ASTBuilding.ASTStructures.Expressions.ClassInstanceCreationNode;
 import Joosc.ASTBuilding.ASTStructures.Expressions.ExpressionNode;
 import Joosc.ASTModel.AST;
 
@@ -8,7 +7,6 @@ public abstract class Expression extends AST {
 
     public static Expression convertExpressionNode(ExpressionNode node) {
         //TODO
-        if (node == null) return null;
-        return new ClassInstanceCreation((ClassInstanceCreationNode) node);
+        return null;
     }
 }
--- A1/src/Joosc/ASTModel/Expressions/ExpressionClassInstanceCreation.java
+++ A1/src/Joosc/ASTModel/Expressions/ExpressionClassInstanceCreation.java
@@ -1,9 +1,26 @@
 package Joosc.ASTModel.Expressions;
 
-import Joosc.ASTBuilding.ASTStructures.Expressions.ClassInstanceCreationNode;
+import Joosc.ASTBuilding.ASTStructures.Expressions.ExpressionClassInstanceCreationNodeNode;
+import Joosc.ASTModel.Type;
 
-public class ClassInstanceCreation extends Expression {
-    public ClassInstanceCreation(ClassInstanceCreationNode node) {
-        // TODO
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class ExpressionClassInstanceCreation extends Expression {
+    private Type classType;
+    private ArrayList<Expression> argList;
+
+    public ExpressionClassInstanceCreation(ExpressionClassInstanceCreationNodeNode node) {
+        classType = Type.convertTypeNode(node.getClassType());
+        argList = node.getArgList().stream().map(expr -> Expression.convertExpressionNode(expr))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public Type getClassType() {
+        return classType;
+    }
+
+    public ArrayList<Expression> getArgList() {
+        return argList;
     }
 }
