05d011eaf3fd866dea3c2d6b9b77666737df97d0
flycounter <flycounter@hotmail.com>
2019-02-28 19:33:38 -0500

    Changes in unary expressions and casting

--- A1/src/Joosc/ASTBuilding/ASTStructures/Expressions/ExpressionUnaryNode.java
+++ A1/src/Joosc/ASTBuilding/ASTStructures/Expressions/ExpressionUnaryNode.java
@@ -1,7 +1,9 @@
 package Joosc.ASTBuilding.ASTStructures.Expressions;
 
+import Joosc.ASTBuilding.ASTStructures.TypeNode;
 import Joosc.ASTBuilding.Constants.RecursionResolve;
 import Joosc.ASTBuilding.Constants.Symbol;
+import Joosc.ASTModel.Expressions.Expression;
 import Joosc.Exceptions.ASTException;
 import Joosc.Exceptions.WeedingFailureException;
 import Joosc.Parser.LRGrammar.ParseTree;
@@ -13,41 +15,24 @@ public class ExpressionUnaryNode extends ExpressionNode {
     private Symbol kind;
     private Symbol unaryOperator;
     private ExpressionNode targetNode;
-    private ArrayList<String> castingName;
-    private boolean castingIsArray = false;
+    private TypeNode castingTypeNode;
 
     public ExpressionUnaryNode(ParseTree parseTree) throws ASTException {
         this.parseTree = parseTree;
         kind = parseTree.getKind();
-        castingName = new ArrayList<>();
-        targetNode = ExpressionNode.resolveExpressionNode(parseTree.getChildren().get(parseTree.getChildren().size() - 1));
         if (kind.equals(Symbol.CastExpression)) {
-            if (parseTree.getChildren().size() == 6) {
-                castingIsArray = true;
-            }
-            ParseTree cursor = parseTree.getChildren().get(1);
-            if (cursor.getKind().equals(Symbol.Expression)) {
-                cursor = findName(cursor);
-            }
-            if (cursor.getKind().equals(Symbol.PrimitiveType)) {
-                cursor = cursor.getChildren().get(0);
-                if (cursor.getKind().equals(Symbol.Boolean)) {
-                    castingName.add(cursor.getLexeme());
-                } else if (cursor.getKind().equals(Symbol.NumericType)) {
-                    cursor = cursor.getChildren().get(0);
-                    castingName.add(cursor.getLexeme());
-                }
-            }
-            if (cursor.getKind().equals(Symbol.Name)) {
-                RecursionResolve.resolveName(cursor, castingName);
+            castingTypeNode = new TypeNode(parseTree.getChild(1));
+            if (parseTree.getChild(2).getKind().equals(Symbol.LeftSquare)){
+                castingTypeNode.changeToArrayType();
+                targetNode = ExpressionNode.resolveExpressionNode(parseTree.getChild(5));
+            } else {
+                targetNode = ExpressionNode.resolveExpressionNode(parseTree.getChild(3));
             }
         } else {
+            kind = Symbol.UnaryExpression;
             unaryOperator = parseTree.getChildren().get(0).getKind();
+            targetNode = ExpressionNode.resolveExpressionNode(parseTree.getChild(1));
         }
-    }
-
-    ParseTree findName(ParseTree p) {
-        return p.getKind().equals(Symbol.Name) ? p : findName(p.getChildren().get(0));
     }
 
     @Override
@@ -57,21 +42,26 @@ public class ExpressionUnaryNode extends ExpressionNode {
 
     @Override
     public void printInfo(int level) {
-        String title = "Unary Expression: ";
-        if (kind.equals(Symbol.CastExpression)) {
-            title = title + castingName.get(0) + castingName.subList(1, castingName.size()).stream().reduce("", (a, b) -> a + "." + b);
-        }
+        String title = kind.equals(Symbol.CastExpression) ? "Cast Expression:" : "Unary Expression: ";
         printInfoInit(title, level);
-        printInfoSingle("Operator: ", unaryOperator);
-        targetNode.printInfo(level + 1);
+        if (kind.equals(Symbol.CastExpression)) {
+            printInfoStrAtLevel("Casting Type", level + 1);
+            castingTypeNode.printInfo(level + 2);
+            printInfoStrAtLevel("Target Node", level + 1);
+            targetNode.printInfo(level + 2);
+        } else {
+            printInfoSingle("Operator: ", unaryOperator);
+            printInfoStrAtLevel("Target Node:", level + 1);
+            targetNode.printInfo(level + 2);
+        }
     }
 
     public Symbol getKind() {
         return kind;
     }
 
-    public ArrayList<String> getCastingName() {
-        return castingName;
+    public TypeNode getCastingTypeNode() {
+        return castingTypeNode;
     }
 
     public ExpressionNode getTargetNode() {
