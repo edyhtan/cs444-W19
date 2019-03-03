2a023a61d6c29d2f9ee1fe95079562dfc1b91001
Suri Zhang <surizhang1224@gmail.com>
2019-03-03 12:31:48 -0500

    122 passed

--- A1/src/Joosc/Environment/ClassEnv.java
+++ A1/src/Joosc/Environment/ClassEnv.java
@@ -6,10 +6,7 @@ import Joosc.ASTModel.FieldDeclr;
 import Joosc.ASTModel.Program;
 import Joosc.Exceptions.NamingResolveException;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
 public class ClassEnv implements Env {
     TypeDeclr typeDeclr;
@@ -25,45 +22,87 @@ public class ClassEnv implements Env {
         this.program = program;
     }
 
-
-    public void check() throws NamingResolveException {
+    @Override
+    public void resolveImports() throws NamingResolveException {
         HashMap<String, GlobalEnv.PackageNames> packages = parent.packageNames;
         ArrayList<String> canonicalName = typeDeclr.getCanonicalName();
         String simpleName = typeDeclr.getSimpleName();
         ArrayList<ArrayList<String>> singleTypeImports = program.getSingleTypeImport();
         ArrayList<ArrayList<String>> onDemandTypeImports = program.getOnDemandTypeImport();
-        ArrayList<String> parentClass = typeDeclr.getParentClass();
-        ArrayList<ArrayList<String>> extendsInterface = typeDeclr.getExtendsInterface();
 
-        HashSet<String> checkSet = new HashSet<>();
+        // DEBUG:
+        System.out.println("Class: " + simpleName);
+        System.out.println("SingleTypeImports:");
+        singleTypeImports.forEach(x -> {
+            System.out.println("\t" + String.join(".", x));
+        });
+        System.out.println("onDemandTypeImports:");
+        onDemandTypeImports.forEach(x -> {
+            System.out.println("\t" + String.join(".", x));
+        });
+        System.out.println("----------------------------");
+
+
+        HashSet<String> singleImportSet = new HashSet<>();
+        HashSet<String> onDemandImportSet = new HashSet<>();
 
         // clash with class/interface declr
         for (ArrayList<String> singleImport : singleTypeImports) {
-            if(singleImport.equals(canonicalName)) throw new NamingResolveException();
+            HashSet<String> temp = new HashSet<>(singleImport);
+            if (temp.contains(simpleName) && !singleImport.equals(canonicalName))
+                throw new NamingResolveException(String.format("single type import %s clashes with type declr",
+                        String.join(".", singleImport)));
         }
 
-        // no two imports clash
+        // no two singTypeImports clash
         for (ArrayList<String> singleImport : singleTypeImports) {
             String temp = String.join(". ", singleImport);
-            if (checkSet.contains(temp)) throw new NamingResolveException();
-            else checkSet.add(temp);
+            if (singleImportSet.contains(temp))
+                throw new NamingResolveException(String.format("single type import %s clashes with another single type import", temp));
+            else singleImportSet.add(temp);
         }
 
-        // check all import-on-demand matches with package structure
-        for (ArrayList<String> onDemandImport : onDemandTypeImports) {
-            for (Map.Entry<String, GlobalEnv.PackageNames> packageNamesEntry : packages.entrySet()) {
-                if(!matchPackage(packageNamesEntry.getValue(), onDemandImport)) throw new NamingResolveException();
+        // no two onDemandTypeImports clash
+        for(ArrayList<String> onDemandImport : onDemandTypeImports) {
+            String temp = new StringBuilder(String.join(".", onDemandImport)).append(".*").toString();
+            if(onDemandImportSet.contains(temp))
+                throw new NamingResolveException(String.format("single type import %s clashes with another single type import", temp));
+            else onDemandImportSet.add(temp);
+        }
+
+        // singleType clash with onDemand
+        HashSet<String> checkSet = new HashSet<>();
+        for (ArrayList<String> singleImport : singleTypeImports) {
+            singleImport.forEach((x) -> checkSet.add(x));
+        }
+        for (ArrayList<String> onDemand : onDemandTypeImports) {
+            for (String im : onDemand) {
+                // DEBUG
+                System.out.println(im);
+
+                if (checkSet.contains(im))
+                    throw new NamingResolveException("single type import clashes with on demand import at "+ im);
             }
         }
 
-        
+        // check all import-on-demand matches with package structure
+//        if (packages.isEmpty() && !onDemandTypeImports.isEmpty())
+//            throw new NamingResolveException("no package structure found");
+        for (ArrayList<String> onDemandImport : onDemandTypeImports) {
+            for (Map.Entry<String, GlobalEnv.PackageNames> packageNamesEntry : packages.entrySet()) {
+                if (!matchPackage(packageNamesEntry.getValue(), onDemandImport))
+                    throw new NamingResolveException(String.format("on demand import %s does not match any package structure",
+                            String.join(".", onDemandImport)));
+            }
+        }
 
     }
 
     private boolean matchPackage(GlobalEnv.PackageNames packageNames, ArrayList<String> targets) {
+        if(packageNames == null) return false;
         int i = 0;
         while (i < targets.size()) {
-            if(packageNames.nameEquals(targets.get(i))) {
+            if (packageNames.nameEquals(targets.get(i))) {
                 i++;
             } else break;
         }
@@ -71,14 +110,14 @@ public class ClassEnv implements Env {
     }
 
     // return true if there is a duplicated
-    private boolean duplicatedFieldName() throws NamingResolveException{
+    private boolean duplicatedFieldName() throws NamingResolveException {
         if (typeDeclr instanceof ClassDeclr) {
             ArrayList<FieldDeclr> fieldDeclrs = ((ClassDeclr) typeDeclr).getFields();
             for (FieldDeclr fieldDeclr : fieldDeclrs) {
                 if (!fields.contains(fieldDeclr.getName())) {
                     fields.add(fieldDeclr.getName());
                 } else {
-                    throw new NamingResolveException();
+                    throw new NamingResolveException("found more than one field with name " + fieldDeclr.getName());
                 }
             }
         }
