2a674ed0a34030a5d13d65630cd6a9fa7659bfbc
Suri Zhang <surizhang1224@gmail.com>
2019-03-03 22:21:28 -0500

    merge from AST

--- A1/src/Joosc/Joosc.java
+++ A1/src/Joosc/Joosc.java
@@ -24,61 +24,25 @@ public class Joosc {
         return temp[temp.length - 1];
     }
 
-    private static JoosAST process(String filename) {
-        JoosAST ast = null;
+    private static JoosAST process(String filename) throws Exception {
+        JoosScan scan = new JoosScan(new File(filename));
+        String file = getFileName(filename);
+        scan.scan();
+        ArrayList<Token> tokens = scan.getOutput();
 
-        try {
-            JoosScan scan = new JoosScan(new File(filename));
-            String file = getFileName(filename);
-            scan.scan();
-            ArrayList<Token> tokens = scan.getOutput();
+        // Parsing
+        JoosParse parse = new JoosParse();
+        parse.parse(tokens);
+        ParseTree tree = parse.getTree();
 
-            // Parsing
-            JoosParse parse = new JoosParse();
-            parse.parse(tokens);
-            ParseTree tree = parse.getTree();
+        // AST and weeding
+//            tree.print();
+        JoosAST ast = new JoosAST(tree);
+        ast.checkFileName(file);
+//            System.out.println("\n============   AST   ============\n");
+//            ast.printASTInfo();
+        ast.weed();
 
-            ast = new JoosAST(tree);
-            ast.checkFileName(file);
-            ast.printASTInfo();
-            ast.weed();
-        } catch (FileNotFoundException e) {
-            System.err.printf("ERROR: file not found: %s\n", e.getLocalizedMessage());
-            System.exit(2);
-        } catch (InvalidCharacterException e) {
-            System.err.printf("ERROR: invalid characters %s\n", e.getInvalidChar());
-            System.exit(42);
-        } catch (InvalidTokenException e) {
-            System.err.printf("ERROR: invalid lexeme: {%s} (%c, %d)\n", e.getInvalidLexeme(), e.getCurChar(), (int) e.getCurChar());
-            e.printExistingTokens();
-            System.exit(42);
-        } catch (InvalidSyntaxException e) {
-            System.err.printf("ERROR: invalid syntax at %d, on state %d, with input %s\n", e.getLocation(), e.getState(), e.getInput());
-            e.printParseTree();
-            System.exit(42);
-        } catch (InvalidParseTreeStructureException e) {
-            e.printStackTrace();
-            System.err.printf("ERROR: %s", e.getLocalizedMessage());
-            System.exit(42);
-        } catch (InvalidParseTreeException e) {
-            System.err.printf("ERROR: %s\n", e.getLocalizedMessage());
-            System.exit(42);
-        } catch (WrongFileNameException e) {
-            System.err.printf("ERROR: %s\n", e.getLocalizedMessage());
-            System.exit(42);
-        } catch (JoosException e) {
-            e.printStackTrace();
-            System.err.println("Something wrong with your shit...");
-            System.err.printf("ERROR: %s\n", e.getLocalizedMessage());
-            System.exit(42);
-        } catch (WeedingFailureException e) {
-            e.printStackTrace();
-            System.err.println(e.getLocalizedMessage());
-            System.exit(42);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(2);
-        }
         return ast;
     }
 
@@ -87,8 +51,52 @@ public class Joosc {
         IDE_FLAG = !argList.contains("-t");
         argList.remove("-t");
 
-        ArrayList<JoosAST> astList = argList.stream().map(filename -> process(filename))
-                .collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<JoosAST> astList = argList.stream().map(filename ->
+                {
+                    JoosAST ast = null;
+                    try {
+                        ast = process(filename);
+                    } catch (FileNotFoundException e) {
+                        System.err.printf("ERROR: file not found: %s\n", e.getLocalizedMessage());
+                        System.exit(2);
+                    } catch (InvalidCharacterException e) {
+                        System.err.printf("ERROR: invalid characters %s\n", e.getInvalidChar());
+                        System.exit(42);
+                    } catch (InvalidTokenException e) {
+                        System.err.printf("ERROR: invalid lexeme: {%s} (%c, %d)\n", e.getInvalidLexeme(), e.getCurChar(), (int) e.getCurChar());
+                        e.printExistingTokens();
+                        System.exit(42);
+                    } catch (InvalidSyntaxException e) {
+                        System.err.printf("ERROR: invalid syntax at %d, on state %d, with input %s\n", e.getLocation(), e.getState(), e.getInput());
+                        e.printParseTree();
+                        System.exit(42);
+                    } catch (InvalidParseTreeStructureException e) {
+                        e.printStackTrace();
+                        System.err.printf("ERROR: %s", e.getLocalizedMessage());
+                        System.exit(42);
+                    } catch (InvalidParseTreeException e) {
+                        System.err.printf("ERROR: %s\n", e.getLocalizedMessage());
+                        System.exit(42);
+                    } catch (WrongFileNameException e) {
+                        System.err.printf("ERROR: %s\n", e.getLocalizedMessage());
+                        System.exit(42);
+                    } catch (JoosException e) {
+                        e.printStackTrace();
+                        System.err.println("Something wrong with your shit...");
+                        System.err.printf("ERROR: %s\n", e.getLocalizedMessage());
+                        System.exit(42);
+                    } catch (WeedingFailureException e) {
+                        e.printStackTrace();
+                        System.err.println(e.getLocalizedMessage());
+                        System.exit(42);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        System.exit(2);
+                    }
+                    return ast;
+                }
+        ).collect(Collectors.toCollection(ArrayList::new));
+
         ArrayList<Program> asts = astList.stream().map(x -> new Program(x.getRoot()))
                 .collect(Collectors.toCollection(ArrayList::new));
 
