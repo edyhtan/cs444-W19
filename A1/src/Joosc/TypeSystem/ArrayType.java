3b03444918c78f9a31be7bf692c7c56e8c2060d5
Suri Zhang <surizhang1224@gmail.com>
2019-03-11 16:41:37 -0400

    ArrayType decorator

--- A1/src/Joosc/TypeSystem/ArrayType.java
+++ A1/src/Joosc/TypeSystem/ArrayType.java
@@ -1,4 +1,64 @@
 package Joosc.TypeSystem;
 
-public class ArrayType {
+import Joosc.Environment.ClassEnv;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ArrayType extends JoosType{
+    JoosType joosType;
+
+    public ArrayType(ClassEnv classEnv) {
+        super(classEnv);
+        this.joosType = new JoosType(classEnv);
+    }
+
+    public ArrayType(JoosType joosType) {
+        super(joosType);
+        this.joosType = joosType;
+    }
+
+    public void addParent(ArrayType type) {
+        allParents.put(type.joosType, type.joosType.classEnv);
+    }
+
+    @Override
+    public ArrayList<String> getTypeName() {
+        return joosType.typeName;
+    }
+
+    @Override
+    public boolean isPrimitive() {
+        return isPrimitive(this);
+    }
+
+    @Override
+    public ClassEnv getClassEnv() {
+        return classEnv;
+    }
+
+    @Override
+    public boolean hasParent(JoosType parent) {
+        return allParents.containsKey(parent);
+    }
+
+    @Override
+    public HashMap<JoosType, ClassEnv> getAllParents() {
+        return joosType.allParents;
+    }
+
+
+    // check primitive types
+    public boolean equals(ArrayType arrayType) {
+        return this.joosType.getTypeName().equals(arrayType.joosType.typeName);
+    }
+
+    public boolean isA(ArrayType RHS) {
+        return this.joosType.isA(RHS.joosType);
+    }
+
+    // unit tests
+    public static void printTypes() {
+        JoosType.printTypes();
+    }
 }
