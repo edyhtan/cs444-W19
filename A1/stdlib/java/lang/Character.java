1b6e4309c8c0ac4f6f26cff513faba4da7c231c6
Suri Zhang <surizhang1224@gmail.com>
2019-03-14 21:44:34 -0400

    only keep fields and method sig

--- A1/stdlib/java/lang/Character.java
+++ A1/stdlib/java/lang/Character.java
@@ -2,11 +2,11 @@ package java.lang;
 public final class Character {
     public char value;
     public Character(char i) {
-        value = i;
+
     }
     public Character() {
     }
     public String toString() {
-        return String.valueOf(value);
+
     }
 }
