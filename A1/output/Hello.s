7ec56656621aa54fdf3bf0209e4b1ee6083ac04a
Ye Hua Tan <yh2tan@uwaterloo.ca>
2019-04-03 23:15:13 -0400

    pre SIT

---
+++ A1/output/Hello.s
@@ -0,0 +1,12 @@
+	global __class_Hello
+__class_Hello:
+section .data
+
+		global __ref_SIT_Hello
+	__ref_SIT_Hello		dd 0
+
+		global __ref_PARENTS_Hello
+	__ref_PARENTS_Hello		dd 0
+
+section .text
+
---
+++ A1/output/foo_bar.s
@@ -0,0 +1,12 @@
+	global __class_foo_bar
+__class_foo_bar:
+section .data
+
+		global __ref_SIT_foo_bar
+	__ref_SIT_foo_bar		dd 0
+
+		global __ref_PARENTS_foo_bar
+	__ref_PARENTS_foo_bar		dd 0
+
+section .text
+
