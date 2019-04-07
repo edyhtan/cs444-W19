4579d29e23ccdad183704dbecb9b6b2bcdb12013
Suri Zhang <surizhang1224@gmail.com>
2019-04-07 04:37:38 -0400

    t

--- A1/test/assignment_testcases/a5/J1_toomuchinc.java
+++ A1/test/assignment_testcases/a5/J1_toomuchinc.java
@@ -3,10 +3,10 @@ public class J1_toomuchinc {
     public J1_toomuchinc() {}
     public static int test() {
 	int x = -200000+123;
-	new J1_toomuchinc();
-	x = x+100000;
-	x = 100000+x;
-	new J1_toomuchinc();
+//	new J1_toomuchinc();
+//	x = x+100000;
+//	x = 100000+x;
+//	new J1_toomuchinc();
 	return x;
     }
 }
